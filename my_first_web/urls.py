d5ae0436d8107f8a984f9da8e0bb5a9b60db7f30
flezzzer <141737861+flezzzer@users.noreply.github.com>
2024-02-24 21:10:57 +0300

    New templates and routing

--- my_first_web/urls.py
+++ my_first_web/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'my_first_web'
 
 urlpatterns=[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:course_id>', views.single_course, name="single_course")
 ]
--- my_first_web/views.py
+++ my_first_web/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import http.client
+
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from . import models
 
 # Create your views here.
 
 def index(request):
     courses = models.Course.objects.all()
-    return render(request, 'courses.html', {'courses':courses})
+    return render(request, 'my_first_web/courses.html', {'courses':courses})
 
+
+def single_course(request, course_id):
+    course = get_object_or_404(models.Course, pk=course_id)
+    return render(request, 'my_first_web/single_course.html', {'course': course})
